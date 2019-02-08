58e0a936f8c3eec0c8d871400ce006d34440aab0
Elise Bonner <elise0209@mac.com>
2019-02-08 10:03:44 -0500

    feat: styling and adding modal preview

---
+++ index.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+  // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+  console.log('here')
+  $('.modal-trigger').leanModal();
+});
